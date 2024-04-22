46a5034ebe935a2eab3683990857447b6cd2ec6f
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-04-22 17:32:57 +0800

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { getLocale, message } from '@vscode-use/utils'
 import type { Disposable, ExtensionContext } from 'vscode'
+import { getLocale, message } from '@vscode-use/utils'
 
 export async function activate(context: ExtensionContext) {
   const disposes: Disposable[] = []
-  const lan = getLocale()
+  const lan = getLocale() // 获取本地语言
   const isZh = lan.includes('zh')
-  message.info(isZh ? '你好' : 'Hello')
+  message.info(isZh ? '你好' : 'Hello') // 发出消息弹窗
 
   context.subscriptions.push(...disposes)
 }
