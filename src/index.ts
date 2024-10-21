e03251a89d3da3d1721ec8958a7d640d10ce95fb
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-10-21 14:57:57 +0800

    chore: update deps

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import { createExtension, getLocale, message } from '@vscode-use/utils'
-import type { Disposable } from 'vscode'
 
 export = createExtension(() => {
-  const disposes: Disposable[] = []
   const lan = getLocale() // 获取本地语言
   const isZh = lan.includes('zh')
   message.info(isZh ? '你好' : 'Hello') // 发出消息弹窗
-
-  return disposes
 }, () => {
 
 })
